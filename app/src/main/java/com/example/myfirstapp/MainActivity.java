8ccd48b4c628291968578ab5c1b2377c6d5c9251
Francesco Fiorella <francescofio99@gmail.com>
2020-11-12 23:26:45 +0100

    Sum feature is implemented and minor fix in the id definition.

--- app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.myfirstapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //crea l'oggetto pulsante associandolo tramite id a addBtn
+        Button addBtn = (Button) findViewById(R.id.addBtn);
+        //ogni volta che clicco il pulsante, fai la somma tra i due text
+        addBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //crea gli oggetti associandoli ai text
+                EditText firstNumEditText = (EditText) findViewById(R.id.firstNumEditText);
+                EditText secondNumEditText = (EditText) findViewById(R.id.secondNumEditText);
+                TextView resultTextView = (TextView) findViewById(R.id.resultTextView);
+
+                //definisci le variabili convertendo il text in integer
+                int num1 = Integer.parseInt(firstNumEditText.getText().toString());
+                int num2 = Integer.parseInt(secondNumEditText.getText().toString());
+                int result = num1 + num2;
+                //output
+                resultTextView.setText(result + "");
+            }
+        });
     }
 }
